b6fe44c0a86f59d9c8845a17cc616be0efd54f2d
Abhijeet Ugale <ugaleabhijeet03@gmail.com>
2025-09-22 22:24:04 +0530

    update app,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Listings from "./pages/Listings";
-import Contact from "./pages/contact";
+import Contact from "./pages/Contact";
 import About from "./pages/AboutUs";
 
 /**
